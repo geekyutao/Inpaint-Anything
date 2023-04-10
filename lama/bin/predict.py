7fe07066a7fd98049856f9a4750e8a12d1fd0592
geekyutao <geekyutao@163.com>
2023-04-11 00:16:23 +0800

    lama inpaint

--- lama/bin/predict.py
+++ lama/bin/predict.py
@@ -31,103 +31,66 @@ from torch.utils.data._utils.collate import default_collate
 from saicinpainting.training.data.datasets import make_default_val_dataset
 from saicinpainting.training.trainers import load_checkpoint
 from saicinpainting.utils import register_debug_signal_handlers
-from saicinpainting.evaluation.data import pad_tensor_to_modulo, scale_image
 
 LOGGER = logging.getLogger(__name__)
 
-import imageio
-import PIL.Image as Image
-from skimage.color import rgb2gray
-import torch.nn.functional as F
-
-# if self.scale_factor is not None:
-#     result['image'] = scale_image(result['image'], self.scale_factor)
-#     result['mask'] = scale_image(result['mask'], self.scale_factor)
-
-# if self.pad_out_to_modulo is not None and self.pad_out_to_modulo > 1:
-#     result['image'] = pad_img_to_modulo(result['image'], self.pad_out_to_modulo)
-#     result['mask'] = pad_img_to_modulo(result['mask'], self.pad_out_to_modulo)
-
-
-def load_img(img_path):
-    img = imageio.imread(img_path)
-    img = torch.Tensor(img).float() / 255.
-    return img
-
-def load_mask(mask_path):
-    mask = imageio.imread(mask_path)   # mask must be 255 for hole in this InpaintingModel
-    if len(mask.shape) == 3:
-        mask = rgb2gray(mask)
-    mask = (mask > 0).astype(np.uint8) * 255       # threshold due to interpolation
-    mask = torch.Tensor(mask).float()
-    return mask
-
-def tensor_preprocess(img, mask):
-    return img, mask
-
-def rgb_preprocess(img_path, mask_path):
-    img = load_img(img_path)    # [H, W, 3]
-    mask = load_mask(mask_path) # [H, W]
-    return img, mask
 
 @hydra.main(config_path='../configs/prediction', config_name='default.yaml')
 def main(predict_config: OmegaConf):
     try:
         register_debug_signal_handlers()  # kill -10 <pid> will result in traceback dumped into log
+
         device = torch.device(predict_config.device)
 
         train_config_path = os.path.join(predict_config.model.path, 'config.yaml')
         with open(train_config_path, 'r') as f:
             train_config = OmegaConf.create(yaml.safe_load(f))
-
+        
         train_config.training_model.predict_only = True
         train_config.visualizer.kind = 'noop'
 
+        out_ext = predict_config.get('out_ext', '.png')
+
         checkpoint_path = os.path.join(predict_config.model.path, 
                                        'models', 
-                                       predict_config.model.checkpoint) # path to xxx.ckpt
+                                       predict_config.model.checkpoint)
         model = load_checkpoint(train_config, checkpoint_path, strict=False, map_location='cpu')
         model.freeze()
         if not predict_config.get('refine', False):
             model.to(device)
 
-        # if not predict_config.indir.endswith('/'):
-        #     predict_config.indir += '/'
+        if not predict_config.indir.endswith('/'):
+            predict_config.indir += '/'
 
-        with torch.no_grad():
-            batch = {}
-            batch['image'] = img.permute(2, 0, 1).unsqueeze(0)
-            batch['mask'] = mask[None, None]
-            unpad_to_size = [batch['image'].shape[2],  batch['image'].shape[3]]
-            batch['image'] = pad_tensor_to_modulo(batch['image'], mod)
-            batch['mask'] = pad_tensor_to_modulo(batch['mask'], mod)
-            batch = move_to_device(batch, device)
-            batch['mask'] = (batch['mask'] > 0) * 1
-            # batch['mask'] = (batch['mask'] == 0) * 1
-            '''
-            batch['image'].size(): [1, 3, H, W] # 0. ~ 1.
-            batch['mask'].size(): [1, 1, H, W]  # unmask 0 / mask 1
-            batch['unpad_to_size']: doesn't care
-            '''
-            batch = model(batch)                    
-            cur_res = batch[predict_config.out_key][0].permute(1, 2, 0).detach().cpu().numpy()
-            # unpad_to_size = batch.get('unpad_to_size', None)
-            if unpad_to_size is not None:
-                orig_height, orig_width = unpad_to_size
-                cur_res = cur_res[:orig_height, :orig_width]
-            '''
-            cur_res.shape: (H, W, 3)
-            '''
+        dataset = make_default_val_dataset(predict_config.indir, **predict_config.dataset)
+        for img_i in tqdm.trange(len(dataset)):
+            mask_fname = dataset.mask_filenames[img_i]
+            cur_out_fname = os.path.join(
+                predict_config.outdir, 
+                os.path.splitext(mask_fname[len(predict_config.indir):])[0] + out_ext
+            )
+            os.makedirs(os.path.dirname(cur_out_fname), exist_ok=True)
+            batch = default_collate([dataset[img_i]])
+            if predict_config.get('refine', False):
+                assert 'unpad_to_size' in batch, "Unpadded size is required for the refinement"
+                # image unpadding is taken care of in the refiner, so that output image
+                # is same size as the input image
+                cur_res = refine_predict(batch, model, **predict_config.refiner)
+                cur_res = cur_res[0].permute(1,2,0).detach().cpu().numpy()
+            else:
+                with torch.no_grad():
+                    batch = move_to_device(batch, device)
+                    batch['mask'] = (batch['mask'] > 0) * 1
+                    batch = model(batch)                    
+                    cur_res = batch[predict_config.out_key][0].permute(1, 2, 0).detach().cpu().numpy()
+                    unpad_to_size = batch.get('unpad_to_size', None)
+                    if unpad_to_size is not None:
+                        orig_height, orig_width = unpad_to_size
+                        cur_res = cur_res[:orig_height, :orig_width]
+
             cur_res = np.clip(cur_res * 255, 0, 255).astype('uint8')
             cur_res = cv2.cvtColor(cur_res, cv2.COLOR_RGB2BGR)
-            if not os.path.exists(output_path):
-                os.makedirs(output_path)
-            cur_out_fname = os.path.join(
-                output_path, 
-                'result.png'
-            )
             cv2.imwrite(cur_out_fname, cur_res)
-
 
     except KeyboardInterrupt:
         LOGGER.warning('Interrupted by user')
@@ -136,13 +99,5 @@ def main(predict_config: OmegaConf):
         sys.exit(1)
 
 
-
 if __name__ == '__main__':
-    img_path = '/data1/yutao/projects/Inpaint-Anything/example/example.png'
-    mask_path = '/data1/yutao/projects/Inpaint-Anything/example/example_mask.png'
-    output_path = '/data1/yutao/projects/IAM/lama/myoutputs'
-    img, mask = rgb_preprocess(img_path, mask_path)
-    mod = 8 #  No need change
-
     main()
-
