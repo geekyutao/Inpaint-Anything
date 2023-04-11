d5b860c2519e524ffcecce96720a903e04ce4c22
RysonFeng <fengruns@mail.ustc.edu.cn>
2023-04-11 18:31:05 +0800

    Preparing web application

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask import url_for
+from markupsafe import escape
+
+app = Flask(__name__)
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
