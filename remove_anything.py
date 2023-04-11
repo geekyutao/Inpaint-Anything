e426d5b78a7cbacd343366d48d906072a923eca1
RysonFeng <fengruns@mail.ustc.edu.cn>
2023-04-12 02:08:36 +0800

    Minor updates

--- remove_anything.py
+++ remove_anything.py
@@ -7,33 +7,10 @@ from pathlib import Path
 from matplotlib import pyplot as plt
 from typing import Any, Dict, List
 
-from segment_anything import SamPredictor, sam_model_registry
+from sam_segment import predict_masks_with_sam
 from lama_inpaint import inpaint_img_with_lama
 from visual_mask_on_img import show_mask, show_points
 from utils import load_img_to_array, save_array_to_img, dilate_mask
-
-
-def predict_masks_with_sam(
-        img: np.ndarray,
-        point_coords: List[List[float]],
-        point_labels: List[int],
-        model_type: str,
-        ckpt_p: str,
-        device="cuda"
-):
-    point_coords = np.array(point_coords)
-    point_labels = np.array(point_labels)
-    sam = sam_model_registry[model_type](checkpoint=ckpt_p)
-    sam.to(device=device)
-    predictor = SamPredictor(sam)
-
-    predictor.set_image(img)
-    masks, scores, logits = predictor.predict(
-        point_coords=point_coords,
-        point_labels=point_labels,
-        multimask_output=True,
-    )
-    return masks, scores, logits
 
 
 def setup_args(parser):
