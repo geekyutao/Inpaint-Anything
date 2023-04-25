be99128c50f441dd2d42d0fd9e94a86255a2c3d4
jinming liu <jmliu206@gmail.com>
2023-04-25 14:10:34 +0800

    update lama_inpaint

--- lama_inpaint.py
+++ lama_inpaint.py
@@ -81,6 +81,77 @@ def inpaint_img_with_lama(
     cur_res = np.clip(cur_res * 255, 0, 255).astype('uint8')
     return cur_res
 
+
+def build_lama_model(        
+        config_p: str,
+        ckpt_p: str,
+        device="cuda"
+):
+    predict_config = OmegaConf.load(config_p)
+    predict_config.model.path = ckpt_p
+    # device = torch.device(predict_config.device)
+    device = torch.device(device)
+
+    train_config_path = os.path.join(
+        predict_config.model.path, 'config.yaml')
+
+    with open(train_config_path, 'r') as f:
+        train_config = OmegaConf.create(yaml.safe_load(f))
+
+    train_config.training_model.predict_only = True
+    train_config.visualizer.kind = 'noop'
+
+    checkpoint_path = os.path.join(
+        predict_config.model.path, 'models',
+        predict_config.model.checkpoint
+    )
+    model = load_checkpoint(
+        train_config, checkpoint_path, strict=False, map_location=device)
+    model.freeze()
+    if not predict_config.get('refine', False):
+        model.to(device)
+
+    return model
+
+
+@torch.no_grad()
+def inpaint_img_with_builded_lama(
+        model,
+        img: np.ndarray,
+        mask: np.ndarray,
+        config_p: str,
+        mod=8,
+        device="cuda"
+):
+    assert len(mask.shape) == 2
+    if np.max(mask) == 1:
+        mask = mask * 255
+    img = torch.from_numpy(img).float().div(255.)
+    mask = torch.from_numpy(mask).float()
+    predict_config = OmegaConf.load(config_p)
+
+    batch = {}
+    batch['image'] = img.permute(2, 0, 1).unsqueeze(0)
+    batch['mask'] = mask[None, None]
+    unpad_to_size = [batch['image'].shape[2], batch['image'].shape[3]]
+    batch['image'] = pad_tensor_to_modulo(batch['image'], mod)
+    batch['mask'] = pad_tensor_to_modulo(batch['mask'], mod)
+    batch = move_to_device(batch, device)
+    batch['mask'] = (batch['mask'] > 0) * 1
+
+    batch = model(batch)
+    cur_res = batch[predict_config.out_key][0].permute(1, 2, 0)
+    cur_res = cur_res.detach().cpu().numpy()
+
+    if unpad_to_size is not None:
+        orig_height, orig_width = unpad_to_size
+        cur_res = cur_res[:orig_height, :orig_width]
+
+    cur_res = np.clip(cur_res * 255, 0, 255).astype('uint8')
+    return cur_res
+
+
+
 def setup_args(parser):
     parser.add_argument(
         "--input_img", type=str, required=True,
